a496f90532dbfe0e1364e6baacf7502d19a74824
arnavchawla15 <81763124+arnavchawla15@users.noreply.github.com>
2021-05-09 12:57:54 +0530

    Update Player.js

--- Player.js
+++ Player.js
@@ -3,7 +3,7 @@ class Player{
         this.x = x;
         this,y = y;
         this.spt = createSprite(this.x,this.y,50,50);
-        this.spt.shapeColour = "orange";
+        this.spt.shapeColor = "orange";
         this.spt.addAnimation("player",playerAnimation);
         this.spt.scale = 0.1;
     }
